95fa3018d3be6d8a0632cd31f932879c423a0636
Bryan <lagarzab@gmail.com>
2020-03-30 22:05:15 -0400

    Initial component build for carousel

---
+++ components/bottomNavigation.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class='bottomNav'>
+        <div
+            v-if='errorMessage'
+            style='background-color:red; font-weight:bold; color:white;'
+        >{{ errorMessage }}</div>
+        <template v-for='i in slideCount'>
+            <a
+                :key='i-1'
+                href='#'
+                class='navDot'
+                @click='$emit("navToSlide", i-1)'
+                :style='i-1 == selectedSlide ? greyDot : ""'
+            >
+                <q-icon name='lens'></q-icon>
+            </a>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'slideCount', 'selectedSlide', 'errorMessage'
+        ],
+        data () {
+            return {
+                greyDot: 'color: rgba(0, 0, 0, 0.5);'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .bottomNav {
+        text-align: center;
+        width: 100%;
+        align-self: flex-end;
+    }
+    .navDot {
+        background-color: transparent;
+        color: white;
+        text-decoration: none;
+        margin: 8px;
+    }
+</style>
---
+++ components/sideButton.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <a
+            href='#'
+            class='arrowBtn'
+            @click='$emit("navigation", nextValue)'
+        >
+            <div
+                class='arrowNav'
+                :style='"float:" + floatDirection + ";"'
+            >
+                <div>
+                    <q-icon :name='iconName'></q-icon>
+                </div>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'side-button',
+    data () {
+        return {
+
+        }
+    },
+    props: [
+        'sideButtonType' //this should be passed as before or next
+    ],
+    computed: {
+        iconName () {
+            // should return navigate_before || navigate_next
+            return 'navigate_' + this.sideButtonType
+        },
+        floatDirection () {
+            return this.sideButtonType === 'before' ? 'left' : 'right'
+        },
+        nextValue () {
+            return this.sideButtonType === 'before' ? -1 : 1
+        }
+    },
+    rotate (val) {
+        this.$emit('navigation', this.nextValue)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .arrowNav {
+        position: relative;
+        font-size: 100px;
+        height: 100vh;
+        display: flex;
+        flex-grow: 1;
+        width: 7vw;
+        align-items: center;
+        justify-content: center;
+        color: white;
+    }
+    .arrowNav:hover {
+        background-color: rgba(black, 0.25);
+    }
+
+</style>
